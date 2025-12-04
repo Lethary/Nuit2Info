d3b8b19958e5954cd105d8ba07c8827407a135c8
Lethary <boudetk@saintjo.org>
2025-12-04 22:19:45 +0100

    dfgh

--- index.php
+++ index.php
@@ -1,11 +1,4 @@
 <?php
-include 'config.php';
-$db = new PDO(DNS, LOGIN, PASSWORD, $options);
-
-$sql = 'SELECT * FROM ni_afficher';
-$statement = $db->query($sql);
-$results = $statement->fetchAll();
-foreach ($results as $row) {
-    echo 'ID: ' . $row['id'] . ' - Name: ' . $row['name'] . '<br>';
-}
+require_once "/var/www/private/connect.php";
+echo "Connect.php chargé !";
 ?>
