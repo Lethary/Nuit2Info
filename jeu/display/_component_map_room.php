05fa3683123188b705e1cfac0b70b79b31b15323
Nyfos <nyfos@netcourrier.com>
2025-12-05 06:15:14 +0100

    Initial

---
+++ jeu/display/_component_map_room.php
@@ -0,0 +1,18 @@
+<a class="room<?php echo $roomIndex++ ?> <?php if (isSolved($roomId)) echo "solved" ?>" <?php if (!isSolved($roomId)) echo 'href="?rid=' . $roomId . '"' ?>>
+    <div class="d-flex flex-row gap-2 align-items-center">
+        <div class="d-xs-none">
+            <span class="mobile-todo">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-square" viewBox="0 0 16 16">
+                    <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z"/>
+                </svg>
+            </span>
+            <span class="mobile-done">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-square" viewBox="0 0 16 16">
+                    <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z"/>
+                    <path d="M10.97 4.97a.75.75 0 0 1 1.071 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425z"/>
+                </svg>
+            </span>
+        </div>
+        <p class="m-0 w-100 text-xs-center"><?php echo $roomName; ?></p>
+    </div>
+</a>
---
+++ jeu/index.php
@@ -0,0 +1,3 @@
+<?php
+include 'display/main.php';
+?>
